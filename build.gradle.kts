4a73f4c38e6dd7286ef422a7e9e8a17065cc10f2
Christoph Sturm <me@christophsturm.com>
2023-03-25 12:33:04 +0100

    don't build searchable options

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.changelog.markdownToHTML
-import java.util.Locale
 
 fun properties(key: String) = project.findProperty(key).toString()
 
@@ -65,6 +64,9 @@ kover.xmlReport {
 
 
 tasks {
+    buildSearchableOptions {
+        enabled = false
+    }
     wrapper {
         gradleVersion = properties("gradleVersion")
     }
@@ -79,7 +81,7 @@ tasks {
             val start = "<!-- Plugin description -->"
             val end = "<!-- Plugin description end -->"
 
-            with (it.lines()) {
+            with(it.lines()) {
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
@@ -89,20 +91,20 @@ tasks {
 
         val changelog = project.changelog // local variable for configuration cache compatibility
         // Get the latest available change notes from the changelog file
-/*        changeNotes.set(properties("pluginVersion").map { pluginVersion ->
-            with(changelog) {
-                renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
-                        .withHeader(false)
-                        .withEmptySections(false),
-                    Changelog.OutputType.HTML,
-                )
-            }
-        })*/
+        /*        changeNotes.set(properties("pluginVersion").map { pluginVersion ->
+                    with(changelog) {
+                        renderItem(
+                            (getOrNull(pluginVersion) ?: getUnreleased())
+                                .withHeader(false)
+                                .withEmptySections(false),
+                            Changelog.OutputType.HTML,
+                        )
+                    }
+                })*/
     }
 
-    // Configure UI tests plugin
-    // Read more: https://github.com/JetBrains/intellij-ui-test-robot
+// Configure UI tests plugin
+// Read more: https://github.com/JetBrains/intellij-ui-test-robot
     runIdeForUiTests {
         systemProperty("robot-server.port", "8082")
         systemProperty("ide.mac.message.dialogs.as.sheets", "false")
@@ -142,7 +144,7 @@ tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
     rejectVersionIf {
         isNonStable(candidate.version) && !isNonStable(currentVersion)
     }
-    // optional parameters
+// optional parameters
     gradleReleaseChannel = "current"
     checkForGradleUpdate = true
     outputFormatter = "json"
