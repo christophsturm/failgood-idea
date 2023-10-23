cbcbe85f24d68e9a4c0fa28e02a8804a735c20ba
Christoph Sturm <me@christophsturm.com>
2023-10-23 22:46:59 +0200

    add RunConfigurationProducer. wooohoo it works and and can run single tests!

---
+++ src/main/kotlin/failgood/idea/RunConfigurationProducer.kt
@@ -0,0 +1,43 @@
+package failgood.idea
+
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.LazyRunConfigurationProducer
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.execution.junit.JUnitConfiguration
+import com.intellij.execution.junit.JUnitConfigurationType
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.util.Ref
+import com.intellij.psi.PsiElement
+
+private val log = logger<RunConfigurationProducer>()
+
+internal class RunConfigurationProducer : LazyRunConfigurationProducer<JUnitConfiguration>() {
+    override fun getConfigurationFactory(): ConfigurationFactory =
+        JUnitConfigurationType.getInstance().configurationFactories.single()
+
+    override fun setupConfigurationFromContext(
+        configuration: JUnitConfiguration,
+        context: ConfigurationContext,
+        sourceElement: Ref<PsiElement>
+    ): Boolean {
+        val uniqueId = UniqueIdProducer.getUniqueId(sourceElement.get()) ?: return false
+        val data = configuration.persistentData
+        data.setUniqueIds(uniqueId)
+        data.TEST_OBJECT = JUnitConfiguration.TEST_UNIQUE_ID
+        configuration.name = "run $uniqueId"
+        log.warn("setupConfigurationFromContext($configuration, $context, ${sourceElement.get()}")
+        return true
+    }
+
+    override fun isConfigurationFromContext(
+        configuration: JUnitConfiguration,
+        context: ConfigurationContext
+    ): Boolean {
+        // we only care about uniqueid run configs
+        if (configuration.testType != JUnitConfiguration.TEST_UNIQUE_ID) return false
+        val uniqueId = context.psiLocation?.let { UniqueIdProducer.getUniqueId(it) }
+        val b = uniqueId != null && configuration.persistentData.uniqueIds.single() == uniqueId
+        if (b) log.warn("returning true, whooohoo")
+        return b
+    }
+}
--- src/main/kotlin/failgood/idea/RunTestLineMarkerContributor.kt
+++ src/main/kotlin/failgood/idea/RunTestLineMarkerContributor.kt
@@ -4,96 +4,19 @@ import com.intellij.execution.lineMarker.ExecutorAction
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.util.NlsSafe
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.kotlin.psi.KtCallElement
-import org.jetbrains.kotlin.psi.KtClassOrObject
-import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.psi.KtNameReferenceExpression
-import org.jetbrains.kotlin.psi.KtStringTemplateExpression
-import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
 
 private val log = logger<RunTestLineMarkerContributor>()
 
 class RunTestLineMarkerContributor : RunLineMarkerContributor() {
     override fun getInfo(e: PsiElement): Info? {
-        if (e.firstChild != null)
-            return null // "line markers should only be added to leaf elements"
+        val uniqueId = UniqueIdProducer.getUniqueId(e) ?: return null
 
-        val callElement =
-            if (e is KtCallElement) e
-            else
-                e.parent.let {
-                    if (it is KtCallElement) it
-                    else it.parent.let { if (it is KtCallElement) it else return null }
-                }
-
-        val containingClass = callElement.getStrictParentOfType<KtClassOrObject>() ?: return null
-        if (!containingClass.isTestClass()) {
-            return null
-        }
-        val className = containingClass.getQualifiedName()
-        val path = getPathToTest(callElement) ?: return null
-
-        val uniqueId = "[engine:failgood]/[class:${path.first()}($className)]:/[class:${path[1]}]"
         // [engine:failgood]/[class:SingleTestExecutor(failgood.internal.SingleTestExecutorTest)]/[class:test execution]/[method:executes a single test]
         return Info(
             AllIcons.RunConfigurations.TestState.Run,
-            { "run test $uniqueId" },
-            *ExecutorAction.getActions()
+            { "run $uniqueId" },
+            *ExecutorAction.getActions(1)
         )
     }
-
-    private fun getPathToTest(declaration: KtCallElement): List<@NlsSafe String>? {
-        val calleeName = getCalleeName(declaration) ?: return null
-        if (calleeName != "it" && calleeName != "test") return null
-        val testName = getFirstParameter(declaration) ?: return null
-        val parent = declaration.getStrictParentOfType<KtCallElement>()
-        val contextName = getFirstParameter(parent!!) ?: return null
-        return listOf(contextName, testName)
-    }
-
-    private fun getCalleeName(declaration: KtCallElement): String? {
-        val calleeExpression = declaration.calleeExpression
-        return (calleeExpression as? KtNameReferenceExpression)?.getReferencedName()
-    }
-
-    private fun getFirstParameter(declaration: KtCallElement): @NlsSafe String? {
-        val ste =
-            declaration.valueArgumentList?.children?.singleOrNull()?.children?.singleOrNull()
-                ?: return null
-        val ste2 = ste as? KtStringTemplateExpression ?: return null
-        return ste2.entries.joinToString("") { it.text.replace("\\\"", "\"") }
-    }
-}
-
-/** checks if a class is a failgood test class (has a @Test Annotation) */
-private fun KtClassOrObject.isTestClass(): Boolean {
-    val annotationEntries = this.modifierList?.annotationEntries
-    return (annotationEntries?.any { it.shortName?.asString() == "Test" }) == true
-}
-
-// protected method copied from KtClassOrObject.kt
-fun KtClassOrObject.getQualifiedName(): String? {
-    val stub = stub
-    if (stub != null) {
-        val fqName = stub.getFqName()
-        return fqName?.asString()
-    }
-
-    val parts = mutableListOf<String>()
-    var current: KtClassOrObject? = this
-    while (current != null) {
-        val name = current.name ?: return null
-        parts.add(name)
-        current = PsiTreeUtil.getParentOfType(current, KtClassOrObject::class.java)
-    }
-    val file = containingFile as? KtFile ?: return null
-    val fileQualifiedName = file.packageFqName.asString()
-    if (fileQualifiedName.isNotEmpty()) {
-        parts.add(fileQualifiedName)
-    }
-    parts.reverse()
-    return parts.joinToString(separator = ".")
 }
