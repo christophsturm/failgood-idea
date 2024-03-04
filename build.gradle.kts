50840a814cac78f0b8fd287f06d9cd132e238329
Christoph Sturm <me@christophsturm.com>
2024-03-04 23:30:39 +0100

    update build files from template

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import java.util.Locale
 
-fun properties(key: String) = project.findProperty(key).toString()
+fun properties(key: String) = providers.gradleProperty(key)
+
+fun environment(key: String) = providers.environmentVariable(key)
 
 plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
@@ -43,14 +45,15 @@ dependencies {
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
+    pluginName = properties("pluginName")
+    version = properties("platformVersion")
+    type = properties("platformType")
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins.set(
-        properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty)
-    )
+    plugins =
+        properties("platformPlugins").map {
+            it.split(',').map(String::trim).filter(String::isNotEmpty)
+        }
 }
 
 // Configure Gradle Changelog Plugin - read more:
@@ -62,17 +65,17 @@ changelog {
 
 // Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
 qodana {
-    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
-    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
-    saveReport.set(true)
-    showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
+    cachePath = projectDir.resolve(".qodana").canonicalPath
+    reportPath = projectDir.resolve("build/reports/inspections").canonicalPath
+    saveReport = true
+    showReport = environment("QODANA_SHOW_REPORT").map { it.toBoolean() }.getOrElse(false)
 }
 
 koverReport { defaults { xml { onCheck = true } } }
 
 tasks {
     buildSearchableOptions { enabled = false }
-    wrapper { gradleVersion = properties("gradleVersion") }
+    wrapper { gradleVersion = properties("gradleVersion").get() }
 
     patchPluginXml {
         version.set(properties("pluginVersion"))
@@ -111,17 +114,16 @@ tasks {
 
     publishPlugin {
         dependsOn("patchChangelog")
-        token.set(System.getenv("PUBLISH_TOKEN"))
-        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release
+        token = environment("PUBLISH_TOKEN")
+        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release
         // labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel
         // automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels.set(
-            listOf(
-                properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()
-            )
-        )
+        channels =
+            properties("pluginVersion").map {
+                listOf(it.substringAfter('-').substringBefore('.').ifEmpty { "default" })
+            }
     }
     test {
         useJUnitPlatform()
